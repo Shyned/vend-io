afe891b10f9fd3494a07b58ee8df67451c55a28a
Dyshine Hill <eyedolarts@gmail.com>
2022-08-18 13:01:39 -0500

    Found collection config bug still working on the issue

--- src/Components/LoginUser/loginUser.jsx
+++ src/Components/LoginUser/loginUser.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import { useRef } from "react";
-import { auth } from "../../firebaseConfig";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { Navigate } from "react-router-dom";
+// firebase 
+import { collection, addDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth , app , database } from "../../firebaseConfig";
+
 
 export default function LoginUser() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  // firebsae collections
+  const collectionRef = collection(database, "user")
+
+const handlesubmit = ()=> {
+addDoc(collectionRef, {
+  email : emailRef,
+  password : passwordRef
+})
+.then(() => {alert(" Data Added")})
+.catch((err) => { alert(err.message)})
+}
+console.log(emailRef)
+console.log(passwordRef)
+
 
   const signIn = async (e) => {
     e.preventDefault();
@@ -33,7 +50,7 @@ export default function LoginUser() {
       {error && alert(error)}
       <form
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={signIn}
+        onSubmit={handlesubmit}
         id="login-form"
       >
         <label className="block text-gray-700 text-sm font-bold mb-2">
