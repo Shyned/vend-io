64207f8c6cd9e12321b4eb62824b2d904c062dee
Dyshine Hill <eyedolarts@gmail.com>
2022-08-24 23:09:52 -0500

    created backed and frontend folders

--- src/utils/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoute = (props) => {
-  return props.hasUser ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
